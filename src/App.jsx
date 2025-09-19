cb12b59b2b657b90d2e722bdd4875096c56f7e92
Abdelrahman Mohamed <poodymohamed34@gmail.com>
2025-09-20 02:18:49 +0300

    fix: update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,17 @@ export default function App() {
   }
 
   return (
-    <div className="max-w-lg sm:mx-auto mx-2 mt-10 sm:p-6 p-3 bg-gray-50 rounded-2xl shadow-xl">
-      <h1 className="text-3xl font-bold text-center mb-6 text-[#FFC0CB]">
-        ToDo List
-      </h1>
-      <TodoInput onAdd={handleAdd} />
-      <TodoList todos={todos} onToggle={handleToggle} onDelete={handleDelete} />
+    <div className="w-full h-screen pt-20 flex justify-center">
+      <div className="max-w-lg sm:min-w-lg my-auto mx-auto sm:p-6 p-3 bg-gray-50 rounded-2xl shadow-xl relative">
+        <img src="./public/pngtree_romantic-white-daisy-chamomile-flowers_21464345.png" className=" aspect-square sm:w-30 w-20 sm:-top-12 -top-7 sm:-left-12 -left-2 absolute" />
+        <h1 className="text-3xl font-bold text-center mb-6 text-[#FFC0CB]">
+          ToDo List
+        </h1>
+        <TodoInput onAdd={handleAdd} />
+        <TodoList todos={todos} onToggle={handleToggle} onDelete={handleDelete} />
+      </div>
     </div>
+
   )
 }
 
